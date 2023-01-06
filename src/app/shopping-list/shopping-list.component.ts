3217df612dd46a3f6531be1edf5bba2e097bd60e
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-06 09:55:56 +0200

    add ingredient model, shopping edit, recipe-detail

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
+  ingredients : Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatos',10)
+  ];
+
+  constructor(){
+
+  }
+
 
 }
