d6a00714b151b339627314ae0d1aa839123f5f1b
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-18 15:06:01 +0200

    maximilians version

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -3,27 +3,14 @@ import { Directive, HostBinding, HostListener, Input, OnInit } from "@angular/co
 @Directive({
     selector: '[appDropDown]'
 })
-export class DropdownDirective implements OnInit{
+export class DropdownDirective {
 
-    @Input('appDropDown') defaultClass : string ='btn-group';
+    @HostBinding('class.open') isOpen : boolean = false;
 
 
-    @HostBinding('class') customClass : string;
-
-    isOpen : boolean = false;
-
-    @HostListener('click') onClick(){
+    @HostListener('click') toggleOpen(){
         this.isOpen = !this.isOpen;
-        if(this.isOpen){
-            this.customClass = this.defaultClass + ' open';
-        } else{
-            this.customClass = this.defaultClass;
-        }
+        
     }
-
-    ngOnInit(): void {
-        this.customClass = this.defaultClass;
-    }
-
 
 }
